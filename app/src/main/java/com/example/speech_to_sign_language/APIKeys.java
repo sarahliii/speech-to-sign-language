f9af704dfb348e3c6a8465896f4bb520007baf80
Dan_Y <daniel.yd.liu@gmail.com>
2019-12-01 22:25:51 -0800

    Add API to .gitignore

---
+++ app/src/main/java/com/example/speech_to_sign_language/APIKeys.java
@@ -0,0 +1,7 @@
+package com.example.speech_to_sign_language;
+
+public class APIKeys {
+    public static final String speechSubscriptionKey = "YourKeyHere";
+    // Replace below with your own service region (e.g., "westus").
+    public static final String serviceRegion = "Region";
+}
